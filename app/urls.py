0ed437cfd7b823937a2e764ac9c78ebdac74bc45
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-23 18:01:44 +0300

    Add new app view for serving app:query POST requests

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('query/', views.query, name='query'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 def index(request):
     return render(request=request, template_name='app/index.html', context={})
+
+
+def query(request):
+    return HttpResponseRedirect(reverse('app:index'))
