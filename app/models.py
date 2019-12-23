12194c58722049c86f93523c164b23cc74b364c7
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-23 19:47:01 +0300

    Refactoring of Subject model

--- app/models.py
+++ app/models.py
@@ -4,7 +4,6 @@ from django.db import models
 # Create your models here.
 class Subject(models.Model):
     query_string = models.CharField(max_length=100)
-    query_type = models.CharField(max_length=5)
     result = models.CharField(max_length=1000)
 
     def __str__(self):
