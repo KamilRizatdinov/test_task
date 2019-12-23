727aed9d0354de294eac9a444a8b635e2877a960
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-23 23:37:24 +0300

    Removed bug with validation

--- static/app/js/index.js
+++ static/app/js/index.js
@@ -1,14 +1,17 @@
 let query_form = document.getElementById('query');
 let query_type = document.getElementById('query-type');
 let submit_button = document.getElementById('submit-btn');
+let reg = /^\d+$/;
+
 query_form.addEventListener('input', UpdateListener);
 
 function UpdateListener(input) {
-    let length = input.target.value.length;
-    if (length === 13) {
+    let query = input.target.value
+    let length = query.length;
+    if ((length === 13) && reg.exec(query)) {
         query_type.innerText = "OGRN";
         submit_button.disabled = false;
-    } else if (length === 10) {
+    } else if ((length === 10) && reg.exec(query)) {
         query_type.innerText = "INN";
         submit_button.disabled = false;
     } else {
