231d29c5b8eaf7c0155095bda01c7287169deea6
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-23 21:19:30 +0300

    Display last query on the main page

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Subject(models.Model):
     query_string = models.CharField(max_length=13, primary_key=True)
+    request_time = models.DateTimeField(auto_now_add=True)
     result = models.CharField(max_length=1000)
 
     def __str__(self):
--- app/views.py
+++ app/views.py
@@ -1,11 +1,35 @@
+import json
+
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
+import requests
+
+from app.models import Subject
 
 
 # Create your views here.
 def index(request):
-    return render(request=request, template_name='app/index.html', context={})
+    try:
+        subject = Subject.objects.all().order_by('-request_time')[0]
+    except IndexError:
+        subject = None
+
+    return render(request=request,
+                  template_name='app/index.html',
+                  context={'subject': subject})
 
 
 def query(request):
+    endpoint = "https://rmsp.nalog.ru/search-proc.json"
+    query_string = request.POST['query']
+    r = requests.post(url=endpoint, data={'query': query_string, 'mode': 'quick'})
+    json_data = json.loads(r.text)['data']
+
+    if r.status_code != 200 or not json_data:
+        return HttpResponseRedirect(reverse('app:index'))
+    else:
+        subject = Subject(query_string=query_string, result=json_data[0]['name_ex'])
+        subject.save()
+
     return HttpResponseRedirect(reverse('app:index'))
+
