771a662eb8a7bcd601876897ac2596cab35f4e1e
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-23 20:15:00 +0300

    Made query_string a primary key

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Subject(models.Model):
-    query_string = models.CharField(max_length=100)
+    query_string = models.CharField(max_length=13, primary_key=True)
     result = models.CharField(max_length=1000)
 
     def __str__(self):
